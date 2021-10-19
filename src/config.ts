4d142a2bbccdcb5998c03271a7758981b7798081
Hanna <igerne94@gmail.com>
2021-10-19 11:02:19 +0200

    helsedir refsets

--- src/config.ts
+++ src/config.ts
@@ -1,73 +1,31 @@
 //SNOMED CT CONFIGURATIONS
 
 export const terminlogyServer = "https://snowstorm.conteir.no";
-// export const terminlogyServer =
-//   "https://snowstorm-demo.northeurope.cloudapp.azure.com";
 
 export const refsets = [
   {
-    id: "ICPC-2",
-    title: "ICPC-2 - 17551000202109",
-    referenceSet: 17551000202109,
+    id: "Legemiddel",
+    title: "Legemiddel - 12881000202101",
+    referenceSet: 12881000202101,
   },
   {
-    id: "NCxP",
-    title: "NCxP - 123456789",
-    referenceSet: 123456789,
+    id: "Administrasjonsvei",
+    title: "Administrasjonsvei - 23961000202102",
+    referenceSet: 23961000202102,
   },
   {
-    branch: "MAIN/ICPC-2",
-    title: "ICPC-2B",
-    referenceSet: 450993002,
+    id: "Styrkeenhet dosering",
+    title: "Styrkeenhet dosering - 23911000202104",
+    referenceSet: 23911000202104,
   },
+  {
+    id: "Virkestoff",
+    title: "Virkestoff - 23921000202107",
+    referenceSet: 23921000202107,
+  },
+  {
+    id: "Tilstand",
+    title: "Tilstand - 12711000202103",
+    referenceSet: 12711000202103,
+  }
 ];
-
-export const typeSynonimRefsets = [
-  {
-    title: "Norwegian bokmål - 61000202103",
-    referenceSet: 61000202103,
-  },
-  {
-    title: "Fastleger - 123456789",
-    referenceSet: 123456789,
-  },
-  // {
-  //   id: "PREFERRED",
-  //   // title: "NCxP - 123456789",
-  //   referenceSet: 900000000000509007,
-  // },
-  // {
-  //   id: "PREFERRED",
-  //   // title: "NCxP - 123456789",
-  //   referenceSet: 900000000000508004,
-  // },
-  // {
-  //   id: "ACCEPTABLE",
-  //   // title: "NCxP - 123456789",
-  //   referenceSet: 900000000000509007,
-  // },
-  // {
-  //   id: "ACCEPTABLE",
-  //   // title: "NCxP - 123456789",
-  //   referenceSet: 900000000000508004,
-  // }
-];
-
-export const typeStatus = [
-  {
-    id: "ACCEPTABLE",
-    title: "ACCEPTABLE",
-  },
-  {
-    id: "PREFERRED",
-    title: "PREFERRED",
-  },
-];
-
-/*export const params = {
-    method: "GET",
-    headers: {
-      Accept: "application/json",
-      "Ocp-Apim-Subscription-Key": "89b72a3ad5cf4723b3f489c3eb4d82a1",
-    },
-  };*/
