1cba7186c62a2a1a559162369a606437ae2356b7
Hanna <igerne94@gmail.com>
2021-10-31 22:09:04 +0100

    render names

--- src/Components/BulkInterface.jsx
+++ src/Components/BulkInterface.jsx
@@ -14,13 +14,16 @@ export const BulkInterface = class BulkInterface extends React.Component {
       inputFromTextarea: "",
       refset: "",
       requestUrl: "",
+      data: {},
+      dataWithMembers: {},
+      showContent: true,
       headers: {
         // "Access-Control-Allow-Origin": "https://danger-danger.netlify.app",
         Accept: "application/json",
         "Content-Type": "application/json",
-        credentials: "include",
-        SameSite: "None"
-        // Authorization: "Basic Y29udGVpcjpxN25TeHRGdA==",
+        // credentials: "include",
+        // SameSite: "None",
+        Authorization: "Basic Y29udGVpcjpxN25TeHRGdA=="
       },
       showSpinner: false,
     };
@@ -38,73 +41,114 @@ export const BulkInterface = class BulkInterface extends React.Component {
     this.setState({ refset: refset });
   };
 
+  // callPost = (memberForRequest, membersArray) => {
   inputHandler = () => {
-    this.setState({ showSpinner: true });
 
-    let inputFromTextarea = this.state.inputFromTextarea;
-    let splitedPairs = [];
-    let membersArray = [];
-
-    if (!inputFromTextarea) {
-      return console.log("Please, put the list of values!");
-    }
-
-    let pairs = inputFromTextarea.match(/[^\n\r]+/g);
-
-    pairs.forEach((pair) => {
-      let pairArray = pair.split(";");
-
-      pairArray[0] = pairArray[0].trim();
-      pairArray[1] = pairArray[1] || "";
-      pairArray[1] = pairArray[1].trim();
-
-      splitedPairs.push(pairArray);
-    });
-
-    splitedPairs.forEach((pair) => {
       let member = {
         active: true,
-        additionalFields: {
-          mapTarget: pair[1],
-        },
+        // additionalFields: {
+        //   mapTarget: pair[1],
+        // },
         effectiveTime: new Date().toISOString().slice(0, 10).replace(/-/g, ""),
         moduleId: "715152001",
-        referencedComponentId: pair[0],
-        refsetId: this.state.refset,
+        referencedComponentId: this.state.inputFromTextarea,
+        refsetId: this.state.refset
       };
 
-      membersArray.push(member);
-    });
-
-    let memberForRequest = membersArray.shift();
-
-    this.callPost(memberForRequest, membersArray);
-  };
-
-  callPost = (memberForRequest, membersArray) => {
     this.setState({ showSpinner: true });
 
-    let branch = "MAIN/SNOMEDCT-NO/TESTBRANCH";
+    // let branch = "MAIN/SNOMEDCT-NO/TESTBRANCH";
+    let branch = "MAIN/SNOMEDCT-NO/TEST";
+
     let requestUrl = terminlogyServer + "/" + branch + "/members";
 
     const parameters = {
       method: "POST",
       credentials: "include",
       headers: this.state.headers,
-      body: JSON.stringify(memberForRequest),
+      body: JSON.stringify(member),
     };
 
     fetch(requestUrl, parameters)
       .then((response) => response.json())
       .then((data) => {
-        if (membersArray.length > 0) {
-          let nextMember = membersArray.shift();
-          this.callPost(nextMember, membersArray);
+        if (member.length > 0) {
+          // let nextMember = membersArray.shift();
+          this.setState({ data: data, showSpinner: true }); // !!!!!
+          console.log("what is the data here after fetch and set? ", data);
+          this.callPost(member); // Does it need to be here?
         } else {
-          this.setState({ showSpinner: false });
+          console.log("Data here (inside bulk-import): ", data);
+          console.log("refsetId here (inside bulk-import): ", data.refsetId);
+          this.setState({ showSpinner: false, data: data });
         }
+
+        this.getMembers(data);
       });
+
+      
   };
+
+  getMembers = (data) => {
+    this.setState({ showSpinner: true });
+
+    let refsetId = data.refsetId;
+    let referencedComponentId = data.referencedComponentId;
+
+    console.log("data from inputHandler ", data);
+    console.log("refsetId from inputHandler ", refsetId);
+    console.log("referencedComponentId from inputHandler ", referencedComponentId);
+
+
+    let branch = "MAIN/SNOMEDCT-NO/TEST";
+
+    let getMembersRequestUrl = terminlogyServer + "/" + branch + "/members";
+
+    const parameters = {
+      method: "GET",
+      credentials: "include",
+      headers: this.state.headers,
+      // body: JSON.stringify(member),
+    };
+
+    fetch(getMembersRequestUrl, parameters)
+      .then((response) => response.json())
+      .then((dataWithMembers) => {
+
+        // let elem = dataWithMembers.map((item, index)=> {
+        //   return (
+        //     <div key={index}>
+        //       {console.log("refsetId here (inside bulk-import): ", item)}
+        //     </div>
+        //   );
+        // });
+
+        this.setState({ showSpinner: false, dataWithMembers: dataWithMembers, showContent: true });
+        // console.log("dataWithMembers here (inside bulk-import): ", dataWithMembers);
+        // this.showNames(dataWithMembers);
+
+      });
+
+  }
+
+  showNames = () => {
+    // console.log("dataWithMembers here (inside bulk-import): ", dataWithMembers);
+    // let importantData = [];
+
+    if(this.state.dataWithMembers && this.state.dataWithMembers.items) {
+
+      return this.state.dataWithMembers.items.map((item, index) => {
+        return (
+          <div key={index}>
+            {item.referencedComponent.term}
+            {console.log("item.referencedComponent.term: ", item.referencedComponent.term)}
+            {/* {importantData = item.referencedComponent.term} */}
+          </div>
+        );
+      });
+    }
+
+  }
 
   render() {
     return (
@@ -121,43 +165,84 @@ export const BulkInterface = class BulkInterface extends React.Component {
         </header>
 
         <article>
+
           <div className="row form-group">
             
-            <div className="col-md-6">
+            <div className="col-md-8">
 
               <div className="row form-group">
                 <RefsetComponent
                   refsetFromChildToParent={this.callbackRefsetHandler}
                 />
               </div>
-              <div className="row form-group">
-                {/* MAIN/SNOMEDCT-NO/HELSEDIREKTORATET */}
-                  {/* MAIN/SNOMEDCT-NO/TESTBRANCH */}
-              </div>
 
-              <div>
-                <textarea
-                  className="select"
-                  aria-label="Input"
-                  // id="input_id"
-                  type="text"
-                  autoComplete="off"
-                  placeholder="SCTID;mapTarget"
-                  onChange={this.getInput}
-                />
+              <div className="row form-group">
+                
+                <div className="col-md-6">
+                    <textarea
+                      className="select"
+                      aria-label="Input"
+                      // id="input_id"
+                      type="text"
+                      autoComplete="off"
+                      placeholder="SCTID
+                        (
+                          referencedComponentId
+                          )"
+                      onChange={this.getInput}
+                    />
+                </div>
+          
+
+                <div className="col-md-6">
+
+                  {this.state.showContent ? (
+                    <div className="popup">
+
+                      <div className="header">
+                        <span>Refset members' names</span>
+                        <span
+                          className="popup-close"
+                          onClick={() => this.setState({ showContent: false })}
+                        >
+                          X
+                        </span>
+                      </div>
+
+                      <div className="content">
+                        {this.showNames()}
+                      </div>
+                      
+                    </div>
+                  ) : null}
+                  
+                </div>
+
               </div>
 
                 <div>
-                  <button onClick={this.inputHandler}>IMPORT</button>
-              </div>
+                  <button onClick={this.inputHandler}>Add member</button>
+                </div>
+
             </div>
 
-            <div className="col-md-6">
+
+
+
+            <div className="col-md-4">
                 <HowToComponent />
-              </div>
-              {this.state.showSpinner ? <Spinner color="success" /> : null}
             </div>
+
+            {
+              this.state.showSpinner ? 
+                <Spinner color="success" /> 
+              : null
+            }
+
+          </div>
+
         </article>
+
       </div>
     );
   }
--- src/config.ts
+++ src/config.ts
@@ -1,31 +1,37 @@
 //SNOMED CT CONFIGURATIONS
 
-export const terminlogyServer = "https://snowstorm.conteir.no";
+// export const terminlogyServer = "https://snowstorm.conteir.no";
+export const terminlogyServer = "https://seabreeze.conteir.no";
+
 
 export const refsets = [
   {
-    id: "Legemiddel",
-    title: "Legemiddel - 12881000202101",
-    referenceSet: 12881000202101,
-  },
-  {
-    id: "Administrasjonsvei",
-    title: "Administrasjonsvei - 23961000202102",
-    referenceSet: 23961000202102,
-  },
-  {
-    id: "Styrkeenhet dosering",
-    title: "Styrkeenhet dosering - 23911000202104",
-    referenceSet: 23911000202104,
-  },
-  {
-    id: "Virkestoff",
-    title: "Virkestoff - 23921000202107",
-    referenceSet: 23921000202107,
-  },
-  {
-    id: "Tilstand",
-    title: "Tilstand - 12711000202103",
-    referenceSet: 12711000202103,
+    title: "Fastleger (test! To delete later and uncomment the old ones!) - 123456789",
+    referenceSet: 123456789,
   }
+  // {
+  //   id: "Legemiddel",
+  //   title: "Legemiddel - 12881000202101",
+  //   referenceSet: 12881000202101,
+  // },
+  // {
+  //   id: "Administrasjonsvei",
+  //   title: "Administrasjonsvei - 23961000202102",
+  //   referenceSet: 23961000202102,
+  // },
+  // {
+  //   id: "Styrkeenhet dosering",
+  //   title: "Styrkeenhet dosering - 23911000202104",
+  //   referenceSet: 23911000202104,
+  // },
+  // {
+  //   id: "Virkestoff",
+  //   title: "Virkestoff - 23921000202107",
+  //   referenceSet: 23921000202107,
+  // },
+  // {
+  //   id: "Tilstand",
+  //   title: "Tilstand - 12711000202103",
+  //   referenceSet: 12711000202103,
+  // }
 ];
